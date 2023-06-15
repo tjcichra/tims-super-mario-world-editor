d488b3a4dc6088c00d0f6abb6c50d2f3941ff7a4
tjcichra <8120075+tjcichra@users.noreply.github.com>
2023-06-14 22:44:56 -0400

    added file dropdown

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,11 +14,18 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
+	fileDialog := dialog.NewFileOpen(func(uriReadCloser fyne.URIReadCloser, err error) {
+		filePath := uriReadCloser.URI().Path()
+
+		fmt.Println(filePath)
+	}, w)
+
 	hello := widget.NewLabel("Hello Fyne!")
 	w.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("Hi!", func() {
 			hello.SetText("Welcome :)")
+			fileDialog.Show()
 		}),
 	))
 
