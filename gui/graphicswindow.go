caad0c7613e4ed09ba3c8b2338b096c2c4ff2071
tjcichra <8120075+tjcichra@users.noreply.github.com>
2023-06-29 16:05:09 -0400

    gui work

--- gui/graphicswindow.go
+++ gui/graphicswindow.go
@@ -1,1 +1,9 @@
 package gui
+
+import "fyne.io/fyne/v2"
+
+func createGraphicsWindow(app fyne.App) fyne.Window {
+	graphicsWindow := app.NewWindow("Graphics")
+
+	return graphicsWindow
+}
--- gui/mainwindow.go
+++ gui/mainwindow.go
@@ -5,13 +5,14 @@ import (
 	"image"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func createMainWindow(app fyne.App, image *image.Paletted) fyne.Window {
+	// graphicsWindow := createGraphicsWindow(app)
+
 	mainWindow := app.NewWindow("Hello")
 
 	toolbar := widget.NewToolbar(
@@ -25,17 +26,58 @@ func createMainWindow(app fyne.App, image *image.Paletted) fyne.Window {
 		widget.NewToolbarSpacer(),
 		widget.NewToolbarAction(theme.HelpIcon(), func() {
 			fmt.Println("Display help")
+			// graphicsWindow.Show()
 		}),
 	)
+	tree := widget.NewTree(
+		func(id widget.TreeNodeID) []widget.TreeNodeID {
+			switch id {
+			case "":
+				return []widget.TreeNodeID{"levels", "b", "c"}
+			case "levels":
+				return []widget.TreeNodeID{"a1", "a2"}
+			}
+			return []string{}
+		},
+		func(id widget.TreeNodeID) bool {
+			return id == "" || id == "levels"
+		},
+		func(branch bool) fyne.CanvasObject {
+			if branch {
+				return widget.NewLabel("Branch template")
+			}
+			return widget.NewLabel("Leaf template")
+		},
+		func(id widget.TreeNodeID, branch bool, o fyne.CanvasObject) {
+			text := id
+			if branch {
+				text += " (branch)"
+			}
+			o.(*widget.Label).SetText(text)
+		})
 
-	imgComponent := canvas.NewImageFromImage(image)
-	imgComponent.ScaleMode = canvas.ImageScalePixels
+	// tabs := container.NewAppTabs(
+	// 	container.NewTabItem("Tab 1", widget.NewLabel("Hello")),
+	// 	container.NewTabItem("Tab 2", widget.NewLabel("World!")),
+	// )
 
-	hello := widget.NewLabel("Hello Fyne!")
-	mainWindow.SetContent(container.NewBorder(toolbar, nil, nil, nil, container.NewVBox(
-		hello),
-		imgComponent,
-	))
+	tabs := container.NewAppTabs([]*container.TabItem{
+		// container.NewTabItem("Tab 1", container.NewBorder(toolbar, nil, nil, nil, nil)),
+		container.NewTabItem("Tab 2", widget.NewLabel("World!"))}...,
+	)
+
+	// tabs.SetTabLocation(container.TabLocationLeading)
+
+	// imgComponent := canvas.NewImageFromImage(image)
+	// imgComponent.ScaleMode = canvas.ImageScalePixels
+
+	// hello := widget.NewLabel("Hello Fyne!")
+	// mainWindow.SetContent(container.NewBorder(toolbar, nil, nil, nil, container.NewVBox(
+	// 	hello),
+	// // imgComponent,
+	// ))
+
+	mainWindow.SetContent(container.NewBorder(toolbar, nil, tree, nil, tabs)) // imgComponent,
 
 	return mainWindow
 }
